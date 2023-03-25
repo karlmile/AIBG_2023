0f8d363707a58baa67178b7b9ce8ce474a33e637
LMauricius <mauriciosmit100@gmail.com>
2023-03-25 05:08:01 +0100

    Funkcionalno spajanje

--- pyController/controller.py
+++ pyController/controller.py
@@ -35,7 +35,7 @@ def sendRecvJson(url: str, payloadObj: "any|None", bearerKey: str|None = None, r
         # Error handling for failed request
         raise RuntimeError(f"Request failed with status code: {response.status_code}")
 
-async def sendAsyncJson(session, url: str, payloadObj: "any|None", bearerKey: str|None = None):
+def sendAsyncJson(session: FuturesSession, url: str, payloadObj: "any|None", bearerKey: str|None = None):
     headers = {}
     if payloadObj is not None:
         headers["Content-Type"] = "application/json"
@@ -49,30 +49,82 @@ async def sendAsyncJson(session, url: str, payloadObj: "any|None", bearerKey: st
 
 
     if payloadObj is None:
-        async with session.get(url, headers=headers, json=payloadObj) as response:
-            return await response.json()
+        return session.get(url, headers=headers, json=payloadObj)
     else:
-        async with session.post(url, headers=headers, json=payloadObj) as response:
-            return await response.json()
+        return session.post(url, headers=headers, json=payloadObj)
 
-async def recvAsyncJson(promise, returnsMessage=False):
-    responseObj = await promise
+def recvAsyncJson(future, returnsMessage=False):
+    responseObj = json.loads(future.result().text)
     if not returnsMessage and "message" in responseObj:
         raise RuntimeError(f"Request failed with message: {responseObj['message']}")
 
     return responseObj
 
+class Figure:
+    def __init__(self, jsonState: any) -> None:
+        self.vrsta = jsonState["oznaka"]
+        pass
+
+FigCountPerColor = 48
+
+class Board:
+    def __init__(self, jsonState: any, whiteFigs, blackFigs) -> None:
+        self.whiteFigs: list[int] = whiteFigs
+        self.blackFigs: list[int] = blackFigs
+
+        self.fields = [
+            [
+                self.getNewFigInd(fig)
+                for fig in line
+            ]
+            for line in jsonState["board"]
+        ]
+
+        self.whiteCaptured = [
+            self.getNewFigInd(fig)
+            for fig in jsonState["whitePiecesPlacement"]
+        ] + [
+            self.getNewFigInd(fig)
+            for fig in jsonState["whitePijanPlacement"]
+        ]
+
+        self.blackCaptured = [
+            self.getNewFigInd(fig)
+            for fig in jsonState["blackPiecesPlacement"]
+        ] + [
+            self.getNewFigInd(fig)
+            for fig in jsonState["blackPijanPlacement"]
+        ]
+        
+    def getNewFigInd(self, jsonState: "any|None") -> int:
+        if jsonState is None:
+            return -1
+        else:
+            if jsonState["black"] == True:
+                self.blackFigs.append(Figure(jsonState))
+                return len(self.blackFigs)-1 + FigCountPerColor
+            else:
+                self.blackFigs.append(Figure(jsonState))
+                return len(self.blackFigs)-1 + FigCountPerColor
 
 
 class GameState:
     def __init__(self, jsonState: any) -> None:
+        self.whiteFigs = []
+        self.blackFigs = []
+        self.board1 = Board(jsonState["boardState1"], self.whiteFigs, self.blackFigs)
+        self.board2 = Board(jsonState["boardState2"], self.whiteFigs, self.blackFigs)
+        self.turn = 0 if jsonState["boardState1"]["whiteMoves"] == True else 1
+        self.phase = jsonState["boardState1"]["phase"]
+        self.numInvalid = jsonState["illegalMoveCounter"]
         pass
 
 
 
-async def main():
+def main():
     # globals
     gameState: GameState = None
+    asyncSession = FuturesSession()
 
     # read params
     adminToken: str|None = None
@@ -102,61 +154,79 @@ async def main():
         else:
             raise RuntimeError(f"Unknown command {comm}")
         i += 1
-        
-    async with aiohttp.ClientSession() as asyncSession:
 
-        # Create the game
-        if adminToken is None:
-            print("Logging in as admin..."); t=time.time()
-            resp = sendRecvJson(url+"/user/login", {"username": "admin", "password": "admin"})
-            adminToken = resp["token"]
-            print(f"Token: {adminToken}")
-            print(f"Logged in as admin! ({time.time()-t}s)")
-        
-        # Logins
-        playerJoiningHttpRequests = [None, None]
-        playerJoiningTimers = [None, None]
-        for i in playerInds:
-            if playerControllers[i] != "remote":
-                if playerTokens[i] is None:
-                    print(f"Logging in as {playerNames[i]}..."); t=time.time()
-                    resp = sendRecvJson(url+"/user/login", {"username": playerNames[i], "password": playerPasswords[i]})
-                    playerTokens[i] = resp["token"]
-                    print(f"Logged in as {playerNames[i]}! ({time.time()-t}s)")
+    # Create the game
+    if adminToken is None:
+        print("Logging in as admin..."); t=time.time()
+        resp = sendRecvJson(url+"/user/login", {"username": "admin", "password": "admin"})
+        adminToken = resp["token"]
+        print(f"Token: {adminToken}")
+        print(f"Logged in as admin! ({time.time()-t}s)")
+    
+    # Logins
+    playerJoiningHttpRequests = [None, None]
+    playerJoiningTimers = [None, None]
+    for i in playerInds:
+        if playerControllers[i] != "remote":
+            if playerTokens[i] is None:
+                print(f"Logging in as {playerNames[i]}..."); t=time.time()
+                resp = sendRecvJson(url+"/user/login", {"username": playerNames[i], "password": playerPasswords[i]})
+                playerTokens[i] = resp["token"]
+                print(f"Logged in as {playerNames[i]}! ({time.time()-t}s)")
 
-        # Game create
-        if gameId is None:
-            gameId = random.randint(0, 1000000)
-            print(f"Creating game {gameId} as admin..."); t=time.time()
-            resp = sendRecvJson(url+"/game/createGame", {"gameId": gameId, "playerUsernames": playerNames}, adminToken, returnsMessage=True)
-            print(f"Creating game {gameId} as admin! ({time.time()-t}s)")
-            print("Stvaranje igre:", resp["message"])
-        
-        # Game joins
-        playerJoiningHttpRequests = [(None), None]
-        playerJoiningTimers = [None, None]
-        for i in playerInds:
-            if playerControllers[i] != "remote":
-                print(f"Joining game as {playerNames[i]}..."); playerJoiningTimers[i]=time.time()
-                playerJoiningHttpRequests[i] = sendAsyncJson(asyncSession, url+"/game/joinGame", None, playerTokens[i])
+    # Game create
+    if gameId is None:
+        gameId = random.randint(0, 1000000)
+        print(f"Creating game {gameId} as admin..."); t=time.time()
+        resp = sendRecvJson(url+"/game/createGame", {"gameId": gameId, "playerUsernames": playerNames}, adminToken, returnsMessage=True)
+        print(f"Creating game {gameId} as admin! ({time.time()-t}s)")
+        print("Stvaranje igre:", resp["message"])
+    
+    # Game joins
+    playerJoiningHttpRequests = [(None), None]
+    playerJoiningTimers = [None, None]
+    for i in playerInds:
+        if playerControllers[i] != "remote":
+            print(f"Joining game as {playerNames[i]}..."); playerJoiningTimers[i]=time.time()
+            playerJoiningHttpRequests[i] = sendAsyncJson(asyncSession, url+"/game/joinGame", None, playerTokens[i])
 
-        for i in playerInds:
-            if playerControllers[i] != "remote":
-                print(f"Waiting for response for {playerNames[i]}...");
-                resp = await recvAsyncJson(playerJoiningHttpRequests[i])
-                print(f"Joined game as {playerNames[i]}! ({time.time()-playerJoiningTimers[i]}s)")
-                gameState = GameState(resp["gameState"])
+    for i in playerInds:
+        if playerControllers[i] != "remote":
+            print(f"Waiting for response for {playerNames[i]}...");
+            resp = recvAsyncJson(playerJoiningHttpRequests[i])
+            print(f"Joined game as {playerNames[i]}! ({time.time()-playerJoiningTimers[i]}s)")
+            gameState = GameState(json.loads(resp["gameState"]))
 
-        # game loop
-        running = True
-        while running:
-            pass
+    # game loop
+    print("Starting the game loop!")
+    print("-------------------------------------------")
+    running = True
+    while running:
+        print(f"Player {gameState.turn}'s turn!")
+        moveServerStr = ""
+
+        if playerControllers[gameState.turn] == "manual":
+            if gameState.phase == 0:
+                print(f"Enter move (M-oldX-oldY-newX-newY or P-FigType-newX-newY)")
+                moveServerStr = input()
+            elif gameState.phase == 1:
+                print(f"Enter move (P-FigType-newX-newY)")
+                moveServerStr = input()
+        elif playerControllers[gameState.turn] == "bot":
+            raise RuntimeError("Bot not implemented yet!")
+
+
+        if playerControllers[i] != "remote":
+            oldInvalids = gameState.numInvalid
+            resp = sendRecvJson(url+"/game/doAction", {"action": moveServerStr}, playerTokens[gameState.turn])
+            gameState = GameState(json.loads(resp["gameState"]))
+            if gameState.numInvalid > oldInvalids:
+                print("ERROR: Invalid move")
 
 
 if __name__ == "__main__":
     try:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+        main()
     except RuntimeError as e:
-        print("ERROR =:÷×÷×÷×÷×÷;<><>><~~~~~~~        :'(\n")
+        print("ERROR =:÷×÷×÷×÷×÷;<><>><~~~~~~~      :'(\n")
         print(e.args[0])
