238b91995125ab72b0a62645b3265560f4b8185a
Andrija Miocic <andrija.miocic1@gmail.com>
2023-03-25 01:16:53 +0100

    slucajevi poteza

--- cppSimulator/src/board.cpp
+++ cppSimulator/src/board.cpp
@@ -31,14 +31,37 @@ Board potez(Board X, Move M){
 
 void Board::listNextMoves(PlayerID player, PlayerID ploca, Board* outBoards, Move* outMoves, size_t maxOutputs) const{
     int i = 0;
+    const int (*P)[12][12] = boards[ploca];
     while (i < maxOutputs){
         for (int j = 0; j < 96; j++){
             if (ALL[j].player == player && ALL[j].board == ploca){
-                if (ALL[j].Position[0] == -1){
+                if (ALL[j].x == -1){
                     for (int i1 = 0; i1 < 12; i1++){
                         for (int i2 = 0; i2 < 12; i2++){
-                            if 
+                            if ((*P)[i1][i2] == -1){
+                                Move M;
+                                M.board = ploca;
+                                M.x = i1;
+                                M.y = i2;
+                                M.index = j;
+                                outMoves[i] = M;
+                            }
                         }
+                    }
+                }
+                else{
+                    int a = ALL[j].x;
+                    int b = ALL[j].y;
+                    switch (ALL[j].F)
+                    {
+                    case Kralj:
+                        if (x > 0 && y > 0){
+                            
+                        }
+                        break;
+                    
+                    default:
+                        break;
                     }
                 }
             }
