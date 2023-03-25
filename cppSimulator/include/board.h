68cc97b3dbb3b0b313ff66c986a837969ce35c03
Andrija Miocic <andrija.miocic1@gmail.com>
2023-03-25 06:33:39 +0100

    ispravak

--- cppSimulator/include/board.h
+++ cppSimulator/include/board.h
@@ -42,13 +42,13 @@ struct Move{
     int y;
 };
 
+const int EMPTY_FIELD = -1;
+
 struct Board{
     int boards[2][12][12];
-    const int empty = -1;
     figura ALL[96];
     Board potez(Board old, Move M) const;
     void listNextMoves(PlayerID player, PlayerID ploca, Board* outBoards, Move* outMoves, size_t maxOutputs) const;
-    
 };
 
 std::ostream& operator << (std::ostream& o, const Board& b);
